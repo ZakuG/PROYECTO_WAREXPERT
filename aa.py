a3ec77e71fa90c5b74539b32db6a61107c13dadc
ZakuG <lcastrob2@correo.uss.cl>
2025-01-16 13:50:25 -0300

    none

---
+++ aa.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Cargar el archivo Excel
+excel_file = 'LISTABSALE25.xlsx'
+
+try:
+    # Leer el archivo Excel
+    df = pd.read_excel(excel_file)
+    
+    # Modificar los valores de la columna 'Tipo de producto'
+    df["Tipo de producto"] = df["Tipo de producto"].replace({
+        "ELECTRICOS": "ELECTRICO",
+        "carroceria": "CARROCERIA",
+        "EMP.": "EMPAQUETADURA",
+        0: "SIN TIPO",
+        "Sin Tipo": "SIN TIPO"
+    })
+    # Obtener los tipos únicos
+    tipos = set(df["Tipo de producto"])
+    print(tipos)
+
+    # Guardar los cambios en un nuevo archivo si es necesario
+    df.to_excel("LISTABSALE25_actualizado.xlsx", index=False)
+
+except Exception as e:
+    print(f"Se produjo un error: {e}")
